05eed1b773f5a1185bb018de15a5a1356e70b67e
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2024-06-21 10:26:08 +0530

    Prem | WIP: Profile pic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import {AboutCard, BgCard} from "../styles/PortfolioComponents";
+import styled from "styled-components";
+
+const ProfileBox = styled.div`
+    display: block;
+    width: 200px;
+    height: 200px;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background-color: var(--primary-color);
+`;
 
 const About = ({id}) => {
   return (<AboutCard id={id}>
     <BgCard>
-      <h1>Profile section placeholder</h1>
+      <ProfileBox></ProfileBox>
       <p>
         I am a full-stack web developer with a passion for creating responsive web applications. I have experience in
         JavaScript, React, Node.js, and more. I am a quick learner and enjoy working on projects that challenge me to
