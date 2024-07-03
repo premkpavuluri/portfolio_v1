b555fdf5a7b57cc3f07212019a74fbc41b91aebf
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2024-07-03 22:23:55 +0530

    Prem | WIP: Experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,56 @@
 import React from "react";
+import styled from "styled-components";
+import {TitleSection} from "../styles/PortfolioComponents";
+
+const myExperience = [
+  {
+    title: "Web Developer - ThoughtWorks",
+    description: "I'm working as a web developer at ThoughtWorks where I was responsible for building and maintaining web applications.",
+    from: '2023',
+    to: 'Present'
+  },
+  {
+    title: "Intern - ThoughtWorks",
+    description: "I worked as an intern at ThoughtWorks where I was responsible for building and maintaining web applications.",
+    from: '2021',
+    to: '2023'
+  }
+]
+
+const ExperienceBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 5rem;
+`;
+
+const ExperienceItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
+    border: 1px solid black;
+    border-radius: 10px;
+    margin: 1rem;
+`;
+
+const ExperienceCard = ({title,desc,from,to}) => {
+  return (<ExperienceItem>
+    <p>{from} - {to}</p>
+    <h2>{title}</h2>
+    <p>{desc}</p>
+  </ExperienceItem>);
+}
 
 const Experience = ({id}) => {
-  return (<div className={"section"} id={id}>
-    <h1>Experience</h1>
-    <h2>Web Developer - ThoughtWorks</h2>
-    <p>
-      I worked as a web developer at ThoughtWorks where I was responsible for building and maintaining web
-      applications. I'm full-stack developer with experience in JavaScript, React, Node.js, and more.
-    </p>
-  </div>);
+  const expItems = myExperience.map(({title,description,from,to}) => {
+    return <ExperienceCard title={title} desc={description} from={from} to={to}/>
+  });
+
+  return (<ExperienceBox id={id}>
+    <TitleSection>Experience</TitleSection>
+    {expItems}
+  </ExperienceBox>);
 }
 
 export default Experience;
