a7a50fcb0d445985022e7fc3f7d6e8b4c746f729
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2024-07-08 22:48:34 +0530

    Prem | WIP: contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,34 @@
 import React from "react";
-import {SectionWrapper} from "../styles/PortfolioComponents";
+import {SectionWrapper, TitleSection} from "../styles/PortfolioComponents";
+import styled from "styled-components";
+import {sectionLabels} from "../config/Config";
+
+const ContactInfoWrapper = styled.div`
+    background-color: var(--primary-background-color);
+    color: var(--text-color);
+    width: 80%;
+    margin-top: 0.5rem;
+    margin-bottom: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Info = styled.div`
+    color: var(--text-color);
+    font-size: 1.5rem;
+    margin: 0.1rem;
+`;
 
 const Contact = ({id}) => {
+
   return (<SectionWrapper id={id}>
-    <h1>Contact</h1>
-    <p>Email:<a href="mailto:premkumarpavuluri@gmail.com">Email</a></p>
+    <TitleSection>{sectionLabels.CONTACT} ME</TitleSection>
+    <ContactInfoWrapper>
+      <Info>I'm always interested in new opportunities and collaborations.</Info>
+      <Info>Feel free to reach out to me:</Info>
+      <p>Email:<a href="mailto:premkumarpavuluri@gmail.com">Email</a></p>
+    </ContactInfoWrapper>
   </SectionWrapper>);
 }
 
