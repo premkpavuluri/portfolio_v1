798d68733933a1a31c6ee925e1e2f6370259c17a
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2024-07-25 23:11:36 +0530

    Prem | Add: useScreenWidth hook

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import {HeaderBox, NameLogo, NavBar, NavBtn} from "../styles/PortfolioComponents";
 import {config} from "../config/Config";
+import useScreenWidth from "../hooks/useScreenWidth";
 
 const Header = ({onClick, activeSection}) => {
   const {sections, myName} = config;
   console.debug("Header activeSection: ", activeSection);
+  console.log("width: ", useScreenWidth())
 
   const buttons = sections.map((section) => (
     <NavBtn key={section.id} onClick={() => onClick(section.id)}>
